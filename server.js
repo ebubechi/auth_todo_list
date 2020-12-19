475fe16d6f476b7cb8183cf4da7b95a12d4e8cac
ebubechi <emmanuelsonzico@gmail.com>
2020-12-19 09:50:52 +0100

    development config

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
 
 //middleware
 
 app.use(cors());
 app.use(express.json());
 
+
+if(process.env.NODE_ENV === "production"){
+  // server static folder
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 //routes
 
 app.use("/authentication", require("./routes/jwtAuth"));
 
 app.use("/dashboard", require("./routes/dashboard"));
 
-app.listen(5000, () => {
-  console.log(`Server is starting on port 5000`);
+
+app.listen(PORT, () => {
+  console.log(`Server is starting on port ${PORT}`);
 });
